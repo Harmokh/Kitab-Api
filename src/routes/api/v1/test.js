3f01d895109e394b7706f745373549588587c743
ubaidh <obaidh@live.com>
2025-08-20 23:09:37 +0530

    jlkj

--- src/routes/api/v1/test.js
+++ src/routes/api/v1/test.js
@@ -15,7 +15,7 @@ module.exports = function (models, express) {
                     id: '1',
                     title: 'Buzurgo ke Ahwal',
                     author: 'Shaykh al-hadith Mawlana Yousuf Motala',
-                    cover: 'https://firebasestorage.googleapis.com/v0/b/quillblog-f5478.appspot.com/o/pdfs%2FBuzurgon-Ke-Wisal-Ke-Ahwal.jpg?alt=media&token=81c0012a-5be2-4b82-9c24-4a576bf09609',
+                    cover: 'https://github.com/Harmokh/Kitab-Api/blob/main/public/images/9.jpg',
                     description: 'A comprehensive guide to the life and teachings of Shaykh al-hadith Mawlana Yousuf Motala.',
                     books: [
                         { id: '1', title: 'Buzurgo ke Ahwal', language: 'Bangla', url: 'https://github.com/Harmokh/Kitab-Api/blob/main/public/pdfs/Buzurgon%20ke%20Wisal%20ke%20Ahwal%20Bangla%20Book%20(17-10-2019).pdf' },
