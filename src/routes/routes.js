97477a46885fe28e1e8f125e018b84d643f10d6a
waseem.dar@mysetu.com <waseem.dar@mysetu.com>
2025-09-20 16:21:40 +0530

    updated changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-var models = require("../models/api/v1/models.js");
+var models = require("../models/models.js");
 module.exports = function (app, express, routeStart) {
-  app.use(routeStart, require("./api/v1/user.js")(models, express));
+  app.use(routeStart, require("./user.js")(models, express));
 };
