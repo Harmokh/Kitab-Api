74e0977ec083814d54e58b88118fa80a0c5587c8
ubaidh <obaidh@live.com>
2026-01-15 13:18:46 +0530

    s

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -623,7 +623,7 @@ module.exports = (models, router) => {
                 return error(res, "Version not found");
             }
 
-            pdfPath = path.join("uploads", version.PDFPath);
+            pdfPath = path.join("uploads", version.pdfPath);
 
             // Validate PDF existence BEFORE search
             if (!fsSync.existsSync(pdfPath)) {
